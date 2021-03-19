3b8a064ffcf5c4df1281e236b05511894843900f
zheng-yuhan <52132731+zheng-yuhan@users.noreply.github.com>
2021-03-19 17:13:16 +0800

    Update z_discos.m

--- z_discos.m
+++ z_discos.m
@@ -1,4 +1,16 @@
 function R=z_discos(otu)
+%- coefficient of niche breadth
+%USAGE: nB=nicheB(otu,method,e)
+% otu       = input data (rows = objects, cols = variables)
+% method    =  Levins(=1,default);Shannon-WInener(=2) or Smith(=3)
+% e         = enviroment data
+% nB        = result of niche breadth
+% ---Notes:---
+% This function is used to calculation of Coefficient of Niche Breadth Analysis.
+%METHOD = 1, Use Levins method to calculation
+%METHOD = 2, Use Shannon-Wienner method to calculation
+%METHOD = 3, Use Smith method to calculation niche breadth with environment
+%factors
 [a,b]=size(otu);
 for j=1:b
     parfor k=1:b
